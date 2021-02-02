89359dcbcfd68df5f00b8ef799ce9dedb39fcf33
farseen <farseenmanekhan1232@gmail.com>
2021-02-03 00:01:21 +0530

    adding styles to the homepage (incomplete)

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import './menu-item.component.scss';
 
-const MenuItem = ({title}) =>{
-    <div className='menu-item'>
-        <div className='content'>
-            <h1 className='title'>{title.capitilize()}</h1>
-            <span className='subtitle'>SHOP NOW</span>
+const MenuItem = ({title , imageUrl , size}) =>{
+    return(
+        
+        <div className={`menu-item ${size}`}>
+            <div className='background-image'
+            style={{
+                backgroundImage:`url(${imageUrl})`
+            }}/>
+            <div className='content'>
+                <h1 className='title'>{title.toUpperCase()}</h1>
+                <span className='subtitle'>SHOP NOW</span>
+            </div>
         </div>
-    </div>
+    );
 }
 
 export default MenuItem;
