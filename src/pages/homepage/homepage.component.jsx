8131099ec22ac005a4f24580eebe7401368ec733
farseen <farseenmanekhan1232@gmail.com>
2021-03-13 16:24:08 +0530

    removing router and switch components again

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import { Route , BrowserRouter as Router,  Switch } from 'react-router-dom';
 import Directory from '../../components/directory.component/directory.component';
 import './homepage.styles.scss';
 
 const HomePage = ()=>{
     return(
-        <Router className='homepage'>
-        <Switch>
-            <Route exact path='/' component={Directory}></Route>
-        </Switch>
-            </Router>
+        <Directory />
     );
 }
 export default HomePage;
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import Categories from "./categories.jsx";
+const Shop = ()=>{
+    return(
+        <Categories />
+    );
+}
