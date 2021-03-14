e91dc734936dad5927c4cbb261cc60b2bba9dae0
farseen <farseenmanekhan1232@gmail.com>
2021-03-14 15:35:30 +0530

    adding react router dom into the project to link shoppage and homepage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import './App.css';
+import {Route , Switch} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component.jsx';
+import Shoppage from './pages/shoppage/shoppage.component.jsx'
 function App() {
-  return (<HomePage />);
+  return (
+    <Switch>
+      <Route exact path='/' component='Homepage' />
+      <Route exact path='/shop' component='Shoppage'/>
+    </Switch>
+    );
 }
 export default App;
--- src/pages/shoppage/shoppage.component.jsx
+++ src/pages/shoppage/shoppage.component.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Category from "../../components/category.component/category.component";
-import SHOP_DATA from "../../";
-class Shoppage extends React.Component {
-    render(
+import SHOP_DATA from "../shoppage/shopdata";
+const Shoppage =()=> {
+    return(
         SHOP_DATA.map((id , title , items)=>{
         
             return(<Category key ={id} title ={title} items= {items}/>);
         
-        });
+        }));
 
 }
 
